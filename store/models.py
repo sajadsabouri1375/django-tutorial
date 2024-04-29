c0fc919fe294fd1a6569e2e85c0ea8fe272268c2
sajad sabouri <sajad.sabouri.temp@gmail.com>
2024-04-30 00:19:08 +0330

    Field slug added to product. one-off default for makemigrations studied.

--- store/models.py
+++ store/models.py
@@ -16,7 +16,8 @@ class Product(models.Model):
     
     # sku = models.CharField(max_length=10, primary_key=True)
     title = models.CharField(max_length=255)
-    description = models.TextField
+    slug = models.SlugField()
+    description = models.TextField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
